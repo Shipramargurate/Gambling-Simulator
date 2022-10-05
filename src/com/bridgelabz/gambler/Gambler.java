ba5a4d025fd50d3069b779183934f0084454a699
shipramargurate <shivanikumari22322@gmail.com>
2022-10-05 16:21:29 +0530

    [shivani] : Added functionlity of bet per day and stake per day

--- src/com/bridgelabz/gambler/Gambler.java
+++ src/com/bridgelabz/gambler/Gambler.java
@@ -1,7 +1,10 @@
 package com.bridgelabz.gambler;
 
 public class Gambler {
+	//starting with a stake of $100 every day and bet $1 every game.
 	public static void main(String[] args) {
-		System.out.println(" welcome to Gambling Simulation Problem");
+		System.out.println("welcome to Gambling Simulator program");
+		int stakePerDay = 100;
+		int betPerGame = 1;
 	}
 }
